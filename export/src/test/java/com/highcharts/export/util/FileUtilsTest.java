272f7740e6df5548d79ec51cc7492107d50f4d4e
xiweicheng <xiweicheng@yeah.net>
2014-05-14 23:26:17 +0800

    ...

---
+++ export/src/test/java/com/highcharts/export/util/FileUtilsTest.java
@@ -0,0 +1,17 @@
+//package com.highcharts.export.util;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.nio.file.Paths;
+//
+//import org.apache.commons.io.FileUtils;
+//import org.junit.Test;
+//
+//public class FileUtilsTest {
+//
+//	@Test
+//	public void copyDirectoryTest() throws IOException {
+//		FileUtils.copyDirectory(new File("C:\\Users\\thinkpad\\Desktop\\dir1"),
+//				Paths.get("C:\\Users\\thinkpad\\Desktop\\dir2", "dir1/").toFile());
+//	}
+// }
